f2c06fc5d8355d15b3027947ef93b2fed7e3c3b3
ThilakRamanie <thilakramanie007@gmail.com>
2024-02-17 23:54:05 +0000

    Initial setup and data rendering --done

---
+++ starter/src/Items.jsx
@@ -0,0 +1,18 @@
+import SingleItem from './SingleItem';
+import { useQuery } from '@tanstack/react-query';
+import customFetch from './utils';
+const Items = () => {
+  const {isLoading, data} = useQuery({
+    queryKey: ['tasks'],
+    queryFn: () => customFetch.get('/')
+  })
+  if(isLoading) return <p style={{marginTop:'1rem'}}>Loading...</p>
+  return (
+    <div className='items'>
+      {data.data.taskList.map((item) => {
+        return <SingleItem key={item.id} item={item} />;
+      })}
+    </div>
+  );
+};
+export default Items;
